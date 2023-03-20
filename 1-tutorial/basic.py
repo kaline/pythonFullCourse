7505c9d1e7db6db185b3079f06d0b612e438d833
kaline <mesquitabfkalinekaline@gmail.com>
2023-03-20 12:49:16 -0300

    add tutorial 1

---
+++ 1-tutorial/basic.py
@@ -0,0 +1,50 @@
+#show hello world message
+print("hello world")
+
+
+# s string is a series of caracters
+print("Kaline B. F Mesquita")
+print('o----')
+print( '||||')
+print('*' * 10)
+
+price = 10
+price = 20
+rating = 4.9
+is_published = True
+print(price)
+
+patient = {'name': 'John Smith', 'age': 20, 'newPatient': True}
+
+print(patient)
+
+#Receiving input 
+
+input('Whats your name?')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
